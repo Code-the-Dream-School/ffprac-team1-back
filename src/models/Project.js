9736e66cac969a9814f6ca5f95f2db996f822ac7
AnnaSolovykh <ms.teachy.solo@gmail.com>
2024-03-08 13:50:08 -0500

    add Project's model

---
+++ src/models/Project.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const TechnologiesSchema = require('./technologiesSchema'); 
+
+const ProjectSchema = new mongoose.Schema({
+  creatorId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User' 
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ["Seeking Team Members", "In Progress", "Completed"],
+    default: "Seeking Team Members" 
+  },
+  technologies: TechnologiesSchema,
+  rolesNeeded: [{
+    type: String,
+    required: true,
+    enum: [
+        'Mentor',
+        'Frontend Developer', 
+        'Backend Developer', 
+        'Fullstack Developer', 
+        'Team Lead',
+        'UI/UX Designer', 
+        'Project Manager', 
+        'DevOps Engineer', 
+        'Quality Assurance Engineer'
+      ]
+  }],
+  likes: {
+    type: Number,
+    default: 0
+  },
+  applicants: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User' 
+  }],
+  participants: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User' 
+  }]
+}, { timestamps: true }); 
+
+const ProjectModel = mongoose.model('Project', ProjectSchema, 'Projects');
+
+module.exports = ProjectModel;
