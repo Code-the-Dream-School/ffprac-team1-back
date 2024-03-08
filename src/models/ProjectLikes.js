888b058860e33a3d5892e89c9ead5cc5b359a2ee
AnnaSolovykh <ms.teachy.solo@gmail.com>
2024-03-08 14:51:08 -0500

    add model for projectLikes

---
+++ src/models/ProjectLikes.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const ProjectLikesSchema = new mongoose.Schema({
+  projectId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Project',
+    required: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  likedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const ProjectLikesModel = mongoose.model('ProjectLike', ProjectLikesSchema, 'ProjectLikes');
+
+module.exports = ProjectLikesModel;
