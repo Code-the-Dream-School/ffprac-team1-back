78560003c49810740881eba3c2d51ab0e8c2687f
AnnaSolovykh <ms.teachy.solo@gmail.com>
2024-03-10 13:56:07 -0400

    delete main controller and router to use futher the joint one named projectsController/router for both auth and unauth access

--- src/app.js
+++ src/app.js
@@ -7,9 +7,8 @@ const cors = require("cors")
 const favicon = require("express-favicon")
 const logger = require("morgan")
 
-const mainRouter = require("./routes/mainRouter")
 const userRouter = require("./routes/userRouter")
-const welcomeRouter = require("./routes/welcomeRouter")
+const projectsRouter = require("./routes/projectsRouter")
 
 // Connect to MongoDB
 const connectDB = require("./db/db")
@@ -24,8 +23,7 @@ app.use(express.static("public"))
 app.use(favicon(__dirname + "/public/favicon.ico"))
 
 // routes
-app.use("/api/v1", mainRouter)
-app.use("/api/v1", welcomeRouter)
+app.use("/api/v1", projectsRouter)
 app.use("/api/v1", userRouter)
 
 // Start the server
--- src/routes/projectsRouter.js
+++ src/routes/projectsRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { displaySearchProjects } = require("../controllers/welcomeController");
+const { displaySearchProjects } = require("../controllers/projectsController");
 
+// GET /api/v1/projects
 router.route("/projects").get(displaySearchProjects);
 
 module.exports = router;
