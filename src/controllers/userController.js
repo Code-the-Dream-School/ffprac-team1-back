05a491da55555441f8f6ce7e8498834c1576563b
Yelena Japarova <yelena.dzhaparova@gmail.com>
2024-03-11 12:19:40 +0100

    basic functionality for login and logout implamented, jwt installed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User.js")
+const bcrypt = require("bcrypt")
 
 const registerUser = async (req, res) => {
   try {
@@ -22,4 +23,29 @@ const registerUser = async (req, res) => {
   }
 }
 
-module.exports = { registerUser }
+const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body
+
+    const user = await User.findOne({ username })
+    if (!user) {
+      return res.status(401).json({ message: "Invalid username or password" })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid username or password" })
+    }
+
+    res.status(200).json({ message: "Login successful" })
+  } catch (error) {
+    console.error("Error logging in:", error)
+    res.status(500).json({ message: "Internal server error" })
+  }
+}
+
+const logoutUser = (req, res) => {
+  res.status(200).json({ message: "Logout successful" })
+}
+
+module.exports = { registerUser, loginUser, logoutUser }
