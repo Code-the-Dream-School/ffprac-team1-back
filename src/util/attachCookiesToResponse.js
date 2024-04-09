8a1cc97430ba2b7e3bc91e4ab82974313833bbf3
AnnaSolovykh <ms.teachy.solo@gmail.com>
2024-04-09 09:25:14 -0400

    fix/allign authentication middleware with cookies setup, fix cookies key to allign with the token key, extend cookies lifetime

--- src/util/attachCookiesToResponse.js
+++ src/util/attachCookiesToResponse.js
@@ -6,11 +6,11 @@ const attachCookiesToResponse = ({ res, user }) => {
   const token = jwt.sign({ userId: user._id }, secret, {
     expiresIn: jwtLifetime
   })
-  const oneDay = 1000 * 60 * 60 * 24
+  const oneWeek = 1000 * 60 * 60 * 24 * 7;
 
   res.cookie("token", token, {
     httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
+    expires: new Date(Date.now() + oneWeek),
     secure: process.env.NODE_ENV === "production",
     signed: true
   })
