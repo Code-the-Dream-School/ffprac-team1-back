0353fa3f2405918e58ec8047b50ad4e85cbbbbaf
AnnaSolovykh <ms.teachy.solo@gmail.com>
2024-04-15 17:04:22 -0400

    add security packages

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const favicon = require("express-favicon")
-const logger = require("morgan")
-const cookieParser = require("cookie-parser")
+require("dotenv").config();
+const express = require("express");
+const app = express();
+//security packages
+const cors = require("cors");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const rateLimiter = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+
+const favicon = require("express-favicon");
 const cloudinary = require('cloudinary').v2;
 
-// middleware
-const errorHandlerMiddleware = require("./middleware/errorHandler")
-const notFoundMiddleware = require("./middleware/notFound")
-
-//import {v2 as cloudinary} from 'cloudinary';
+const xssClean = require('./middleware/xssClean');
+const errorHandlerMiddleware = require("./middleware/errorHandler");
+const notFoundMiddleware = require("./middleware/notFound");
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -20,32 +23,51 @@ cloudinary.config({
 });
 
 
-const userRouter = require("./routes/userRouter")
-const projectsRouter = require("./routes/projectsRouter")
-const profilesRouter = require("./routes/profilesRouter")
+const userRouter = require("./routes/userRouter");
+const projectsRouter = require("./routes/projectsRouter");
+const profilesRouter = require("./routes/profilesRouter");
 
-// Connect to MongoDB
-const connectDB = require("./db/db")
-connectDB()  
+const connectDB = require("./db/db");
+connectDB();
 
 // middleware
+app.use(helmet()); //adds security headers to protect from vulnerabilities
 app.use(cors({
     origin: "http://localhost:3000",
-    credentials: true
-   })); 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(logger("dev"))
-app.use(express.static("public"))
-app.use(favicon(__dirname + "/public/favicon.ico"))
-app.use(cookieParser(process.env.JWT_SECRET)) //the secret key should match the one we sign the cookie with
+    credentials: true //allows sending cookies and credentials from client from specified origins
+})); 
+app.use(
+    rateLimiter({
+      windowMs: 15 * 60 * 1000, //15 minutes -  the time frame for rate limiting
+      max: 100, //limits each IP to 100 requests per window
+    })
+); //limits repeated requests to the endpoints
+app.use(mongoSanitize()); //sanitizes user inputs to prevent MongoDB injection attacks
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false })); //ensures that all incoming request bodies are parsed before any other logic processes them
+app.use(xssClean); //sanitizes user input to prevent XSS attacks by escaping harmful scripts
+app.use(logger("dev"));
+app.use(express.static("public"));
+app.use(favicon(__dirname + "/public/favicon.ico"));
+app.use(cookieParser(process.env.JWT_SECRET)); //the secret key should match the one we sign the cookie with
 
 // routes
-app.use("/api/v1/projects", projectsRouter)
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/profiles", profilesRouter)
+//temporary route to test xssClean
+app.post('/test', (req, res) => {
+    res.send({
+        body: req.body,
+        query: req.query,
+        params: req.params
+    });
+});
 
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
+app.use("/api/v1/projects", projectsRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/profiles", profilesRouter);
 
-module.exports = app
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+module.exports = app;
